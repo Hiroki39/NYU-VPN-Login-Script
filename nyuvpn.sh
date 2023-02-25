dd2ceeb60457351aeabeb9305fb08c8963c5d604
Hiroki39 <zhenghongyi2000@hotmail.com>
2023-02-25 13:41:54 -0500

    script update

--- nyuvpn.sh
+++ nyuvpn.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/expect
-spawn sudo openconnect vpn-gnd.shanghai.nyu.edu # could be substituted by other NYU vpn addresses
+spawn sudo openconnect vpn.nyu.edu # could be substituted by other NYU vpn addresses
 expect "Password:"
 send "<your_computer_root_password>\r"
+expect "GROUP: \\\[NYU VPN: All Traffic|NYU VPN: NYU-NET Traffic Only\\\]:"
+send "NYU VPN: All Traffic\r"
 expect "Username:"
 send "<your_netid>\r"
 expect "Password:"
